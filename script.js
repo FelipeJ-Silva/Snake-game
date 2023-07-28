2fc3c3ddc5155a4698122039f3b38da8a0fab6ef
Felipe J. Silva <138956541+FelipeJ-Silva@users.noreply.github.com>
2023-07-27 23:57:11 -0300

    Continuação do game
    criei as funções da comida, game over e comecei a finalizar o game.

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 
+const audio = new Audio('assets/audio.mp3')
+
 const size = 30
 
 const snake = [{ x: 270, y: 240 }]
 
+const randomNumber = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+const randomPosition = () => {
+    const number = randomNumber(0, canvas.width - size)
+    return Math.round(number / 30) * 30
+}
+
+const randomColor = () => {
+    const red = randomNumber(0,255)
+    const green = randomNumber(0,255)
+    const blue = randomNumber(0,255)
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
+
+
+const food = {
+    x: randomPosition(),
+    y: randomPosition(),
+    color: randomColor()
+}
+
 let direction, loopId
+
+const drawFood = () => {
+
+    const { x, y, color } = food
+
+    ctx.shadowColor = color
+    ctx.shadowBlur = 6
+    ctx.fillStyle = color
+    ctx.fillRect(x, y, size, size)
+    ctx.shadowBlur = 0
+
+}
 
 const drawSnake = () => {
     ctx.fillStyle = "#ddd"
@@ -63,13 +101,57 @@ const drawGrid = () => {
 
 }
 
+const chackEat = () => {
+    const head = snake[snake.length - 1]
+
+    if (head.x == food.x && head.y == food.y) {
+        snake.push(head)
+        audio.play()
+
+        let x = randomPosition()
+        let y = randomPosition()
+
+        while (snake.find((position) => position.x == x && position.y ==y)) {
+            x = randomPosition()
+            y = randomPosition()
+        }
+
+        food.x = x
+        food.y = y
+        food.color = randomColor()
+    }
+}
+
+const checkCollision = () => {
+    const head = snake[snake.length - 1]
+    const canvasLimit = canvas.width - size
+    const neckIndex = snake.length - 2
+
+    const wallCollision = head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit
+
+    const selfCollision = snake.find((position, index) => {
+        return index < neckIndex && position.x == head.x && position.y == head.y
+    })
+
+    if (wallCollision || selfCollision) {
+        gameOver()
+    }
+}
+
+const gameOver = () => {
+    direction = undefined
+}
+
 const gameLoop = () => {
     clearInterval(loopId)
 
     ctx.clearRect(0, 0, 600, 600)
     drawGrid()
+    drawFood()
     moveSnake()
     drawSnake()
+    chackEat()
+    checkCollision()
 
     loopId = setTimeout(() => {
         gameLoop()
